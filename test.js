4bab43e115e29c1794854e602b15ea37e56d334b
KamiSama <31507356+jainsomya972@users.noreply.github.com>
2018-08-07 12:22:24 +0530

    added dialogue construct

--- test.js
+++ test.js
@@ -25,6 +25,143 @@ function processResponse(err, response) {
     return;
   }
   
+  		var intent='irrelevant',state=[],district=[],field=[],area='state',type='Total';
+  		//populating the above variables
+  		if(response.intents.length>0)
+  			intent=response.intents[0].intent;
+  		if(response.entities.length>0)
+  		{
+  			for(var i=0;i<response.entities.length;i++)
+  			{
+  				if(response.entities[i]=='STATE')
+  					state.push(response.entities[i].value);
+  				else if(response.entities[i]=='DISTRICT')
+  					district.push(response.entities[i].value);
+  				else if(response.entities[i]=='FIELD')
+  					field.push(response.entities[i].value);
+  				else if(response.entities[i]=='area')
+  					area = response.entities[i].value;
+  				else
+  					type = response.entities[i].value; 				
+  			}
+  		}
+
+  		//constructing the dialogue
+  		switch(intent)
+  		{
+  			case 'welcome':{
+  				console.log('welcome to jaano india. Ask me anything about indian public data');
+  				break;
+  			};
+  			case 'show':
+  			case 'avg':
+  			case 'irrelevant':{
+  				if(district.length>0)
+  				{
+  					//print field value in district.type
+  				}
+  				else if(state.length>0)
+  				{
+  					//print sum of all district.type fvalues of that state 
+  				}
+  				else
+  				{
+  					//print fvalue of all districts.type in csv
+  				}
+  				break;
+  			};
+  			case 'sum':{
+  				if(district.length>0)
+  				{
+  					//print fvalue of district.type
+  				}
+  				else if(state.length>0)
+  				{
+  					//print sum of fvalues of all districts.type of that state
+  				}
+  				else 
+  				{
+  					//sum of fvalues of all available districts.type
+  				}
+  				break;
+  			};
+  			case 'max':{
+  				if(area=='district'&&state.length>0)
+  				{
+  					//highest fvalue among all ditricts.type of that state
+  				}
+  				else if(area=='district')
+  				{
+  					//highest fvalue among all districts of india
+  				}
+  				else if(area=='state')
+  				{
+  					//highest fvalue among all state.type
+  				}
+  				break;
+  			};
+  			case 'min':{
+  				if(area=='district'&&state.length>0)
+  				{
+  					//lowest fvalue among all ditricts.type of that state
+  				}
+  				else if(area=='district')
+  				{
+  					//lowest fvalue among all districts of india
+  				}
+  				else if(area=='state')
+  				{
+  					//lowest fvalue among all state.type
+  				}
+  				break;
+  			};
+  			case 'greaterthan':{
+  				if(area =='district' && district.length!=0 && field.length == 1)
+  				{
+  					// print all ditricts names whose f.value > district.fvalue
+  					// ex - districts where total poulation > that of jaipur
+  				}
+  				else if(area == 'district' && state.length>0)
+  				{
+  					// print all district names of that state where fvalue1 > fvalue2
+  				}
+  				else if(area =='district')
+  				{
+  					//print all district.type of India where fvalue1 > fvalue2
+  				}
+  				else if(area =='state')
+  				{
+  					// print all state names of India where total fvalue1 > fvalue2
+  				}
+  				else
+  				break;
+  			};
+  			case 'lessthan':{
+  				if(area =='district' && district.length!=0 && field.length == 1)
+  				{
+  					// print all ditricts names whose f.value < district.fvalue
+  					// ex - districts where total poulation < that of jaipur
+  				}
+  				else if(area == 'district' && state.length>0)
+  				{
+  					// print all district names of that state where fvalue1 < fvalue2
+  				}
+  				else if(area =='district')
+  				{
+  					//print all district.type of India where fvalue1 < fvalue2
+  				}
+  				else if(area =='state')
+  				{
+  					// print all state names of India where total fvalue1 < fvalue2
+  				}
+  				else
+  				break;
+  			};
+  			case 'capability':{
+  				console.log(response.output.text[0]);
+  			};
+  			case 'bye':
+  		}
 	 //processing intents
 	  if(response.intents.length>0)
 		console.log("#" + response.intents[0].intent);
