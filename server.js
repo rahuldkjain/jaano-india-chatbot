b41930771f8d73067a06e22b8a6ef44d6ca55b53
Rahul Jain <rahuldkjain@gmail.com>
2018-08-08 12:31:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-
+var http = require('http');
+var fs = require('fs');
+function onRequest(request, response){
+	response.writeHead(200, {'Content-Type':'text/html'});
+  fs.readFile('./index.html',null, function(error,data){
+    if(error){
+      resopnse.writeHead(404);
+      response.write('Page Not Found')
+    } else {
+        response.write(data);
+    }
+  });
+	response.write();
+	response.end();
+}
+http.createServer(onRequest).listen(8000);
